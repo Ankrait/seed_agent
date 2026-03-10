b7b22a0626ab4ee57c8e001da3a95733bb23285f
Azat <azat_fayzrahmanov@mail.ru>
2026-03-10 19:07:51 +0300

    feat: Добавлена память и "human in the loop"

--- llm/openai.py
+++ llm/openai.py
@@ -2,6 +2,7 @@ from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(
     api_key="",
-    base_url="http://127.0.0.1:1234/v1",
-    # base_url="http://172.21.13.70:1234/v1",
+    # base_url="http://127.0.0.1:1234/v1",
+    base_url="https://89fe241d-b6f4-4e49-b3d5-9ed3c91dbe2f.tunnel4.com/v1",
+    # base_url="http://10.128.61.70:1234/v1",
 )
--- main.py
+++ main.py
@@ -3,28 +3,72 @@ from langgraph.checkpoint.memory import InMemorySaver
 
 from agent.graph import build_graph
 
+step = 1
+
+
+def print_chunk_message(chunk):
+    message, meta = chunk
+    global step
+
+    if meta['langgraph_step'] != step:
+        step = meta['langgraph_step']
+        print('\n\nАгент:', end='', flush=False)
+        if meta['langgraph_node'] == 'tool_call':
+            print('*Вызов функции*', end='', flush=False)
+
+    if message.content and meta['langgraph_node'] == 'call_model':
+        print(message.content, end='', flush=False)
+
 
 def main():
     memory = InMemorySaver()
-    agent = build_graph().compile(checkpointer=memory)
+    agent = build_graph().compile(
+        checkpointer=memory, interrupt_before=['tool_call']
+    )
+
+    def run(state: dict | None, config: dict):
+        for chunk in agent.stream(
+            state, config,
+            stream_mode=['updates', 'messages']
+        ):
+            chunk_type, chunk_data = chunk
+            current_state = agent.get_state(config)
+
+            if chunk_type == 'updates':
+                if chunk_data.get('call_model'):
+                    pass
+                if chunk_data.get('__interrupt__') == () and current_state.next == ('tool_call',):
+                    tool_call = (
+                        current_state.values
+                        .get('messages')[-1].tool_calls[0]
+                    )
+
+                    print('\n\n=========================')
+                    print(
+                        f'Вызов {tool_call.get('name')}: {tool_call.get('args')}'
+                    )
+                    answer = input('Подтвердите запуск? (y/n) ')
+                    if answer.lower() == 'y':
+                        run(None, config)
+                    else:
+                        break
+            elif chunk_type == 'messages':
+                print_chunk_message(chunk_data)
 
     while True:
         user = input('\nВы: ')
         if user.lower() == 'exit' or user.lower() == 'выход':
             break
 
-        config = {'configurable': {'thread_id': 'temp'}}
-        answer = agent.invoke(
+        run(
             {
                 "messages": [
                     SystemMessage(f'Ты агент по погоде. Сегодня 19.02.2026'),
                     HumanMessage(user)
                 ]
             },
-            config
+            {'configurable': {'thread_id': 'temp'}}
         )
-
-        print(answer)
 
 
 if __name__ == "__main__":
